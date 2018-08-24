45a9e4df4e80687d7f45b595c0ab2e2aeb931b1d
Geoffrey Migliacci <geoffrey.migliacci@gmail.com>
2018-08-24 16:09:54 +0200

    init file

---
+++ Cambridge.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        Cambridge
+// @namespace   Iterer
+// @version     0.1
+// @description A bombparty simple cheat.
+// @author      Geoffrey Migliacci
+// @match       http://bombparty.sparklinlabs.com/
+// @match       http://bombparty.sparklinlabs.com/play/*
+// @match       http://hub.sparklinlabs.com/apps/BombParty/play/*
+// @require     https://code.jquery.com/jquery-3.3.1.min.js
+// @grant       unsafeWindow
+// ==/UserScript==
+
+$(document).ready(function () {
+    console.log(channel);
+});
