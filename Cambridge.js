4ebf5290de015ec0dedb6749adbf44ac96da9d7d
Geoffrey Migliacci <geoffrey.migliacci@gmail.com>
2018-08-26 11:55:09 +0200

    better version

--- Cambridge.js
+++ Cambridge.js
@@ -19,7 +19,6 @@ $(document).ready(function () {
             success: function (dictionary) {
                 $('#SettingsTab').append('<h2>Cambridge</h2>');
                 $('#SettingsTab').append('<table><tbody><tr><td><button id="CambridgeGame">Game</button></td></tr></tbody></table>');
-                $('#SettingsTab').append('<table><tbody><tr><td><button id="CambridgeChat">Chat</button></td></tr></tbody></table>');
 
                 channel.socket.on("wordRoot", function (a) {
                     channel.data.wordRoot = a.toUpperCase();
@@ -29,29 +28,27 @@ $(document).ready(function () {
                     e.preventDefault();
                     if (channel.data.actors.length) {
                         if (app.user.authId === channel.data.actors[channel.data.activePlayerIndex].authId) {
-                            var word = dictionary.find(function (current) {
-                                return current.indexOf(channel.data.wordRoot) !== -1 && channel.data.actorsByAuthId[app.user.authId].lockedLetters.findIndex(function (letter) {
-                                    return current.indexOf(letter);
+
+                            var WORDS = dictionary.filter(function (a) {
+                                return a.indexOf(channel.data.wordRoot) !== -1;
+                            }).sort(function (a, b) {
+                                return b.length - a.length;
+                            });
+
+                            var WORDS_HEART = WORDS_WITHOUT_LETTERS.filter(function (a) {
+                                return channel.data.actorsByAuthId[app.user.authId].lockedLetters.findIndex(function (b) {
+                                    return a.indexOf(b) !== -1;
                                 }) !== -1;
+                            }).sort(function (a, b) {
+                                return b.length - a.length;
                             });
-
-                            if (word) {
-                                type(word, 1);
-                            }
-                        }
-                    }
-                });
-
-                $('#CambridgeChat').on('click', function (e) {
-                    e.preventDefault();
-                    if (channel.data.actors.length) {
-                        if (app.user.authId === channel.data.actors[channel.data.activePlayerIndex].authId) {
-                            var word = dictionary.find(function (current) {
-                                return current.indexOf(channel.data.wordRoot) !== -1;
-                            });
-
-                            if (word) {
-                                channel.socket.emit("chatMessage", word);
+                            console.log(WORDS_HEART);
+                            console.log(WORDS);
+                            
+                            if (WORDS_HEART.length) {
+                                type(WORDS_HEART[Math.floor(Math.random() * WORDS_HEART.length)], 1);
+                            } else if (WORDS.length) {
+                                type(WORDS[Math.floor(Math.random() * WORDS.length)], 1);
                             }
                         }
                     }
@@ -60,7 +57,7 @@ $(document).ready(function () {
                 function type(string, length) {
                     if (length < string.length + 1) {
                         channel.socket.emit("setWord", { word: string.slice(0, length), validate: string.length === length });
-                        setTimeout(type.bind(null, string, length + 1), 50 + Math.random() * 11);
+                        setTimeout(type.bind(null, string, length + 1), 80 + Math.random() * 16);
                     }
                 }
             }
